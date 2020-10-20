24dd39fada8ae2d3be3a5c6edb31c297b2187c24
tes <tressa0923@gmail.com>
2020-10-20 02:08:10 -0700

    deployment process

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> please deploy portfolio. i beg. </h1>
+    </div>
+  );
+}
+
+export default App;
