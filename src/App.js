167eec814a576e2ef64795121c81cb375bf29015
tes <tressa0923@gmail.com>
2020-11-13 03:32:55 -0800

    following a tutorial to set up the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> please deploy portfolio. i beg. </h1>
-    </div>
+    <React.Fragment>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" />
+      </Switch>
+    </Router>
+      
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+// rfce command to get starter react code
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './styles/Navbar.css';
+
+function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click); // set click state to the opposite
+    const closeMobileMenu = () => setClick(false);
+
+    // If screen is <=960, hamburger 
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+      }, []);
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <React.Fragment>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        ttan <i className="far fa-thumbs-up" />
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                                About Me
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/projects' className="nav-links" onClick={closeMobileMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/contact' className="nav-links" onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {/* If button is true, return Button  */}
+                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} 
+                </div>
+            </nav>
+        </React.Fragment>
+    )
+}
+
+export default Navbar
