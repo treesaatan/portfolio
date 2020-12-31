ec42cdcca8fcde631c59798d737be020307fd5f8
tes <tressa0923@gmail.com>
2020-12-31 03:12:26 -0800

    Updates

---
+++ precache-manifest.0aa7c7323d668dae5846f88a54c7562a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f826edba6c02f15bf86945a0a9fe5530",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bfcfc1641d0212fc93ec",
+    "url": "/portfolio/static/css/main.ad978138.chunk.css"
+  },
+  {
+    "revision": "168d6eda4dae2b954a07",
+    "url": "/portfolio/static/js/2.924cb606.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.924cb606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfcfc1641d0212fc93ec",
+    "url": "/portfolio/static/js/main.fd9b2907.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "481fb3247243f46ab611d96f3b501269",
+    "url": "/portfolio/static/media/craveus_start_card.481fb324.png"
+  },
+  {
+    "revision": "6ddfb9e8e29bc7b9d10ae82bf29a0639",
+    "url": "/portfolio/static/media/post_comment6.0_card.6ddfb9e8.png"
+  },
+  {
+    "revision": "1af4e2ae03ecbec148cc2bbb84f11b50",
+    "url": "/portfolio/static/media/smiley.1af4e2ae.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f812d571f3677c14b17e594c77561618.js"
+  "/portfolio/precache-manifest.0aa7c7323d668dae5846f88a54c7562a.js"
 );
 
 self.addEventListener('message', (event) => {
